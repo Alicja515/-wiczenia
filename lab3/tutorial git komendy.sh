dfc3f284032f01d8ee7d36e90c8a14a8202c2800
Alicja515 <163434659+Alicja515@users.noreply.github.com>
2024-03-17 23:13:47 +0100

    Add files via upload

---
+++ lab3/tutorial git komendy.sh
@@ -0,0 +1,18 @@
+git commit
+git branch
+git checkout
+git merge
+git rebase
+git checkout "nazwa"^
+git checkout HEAD"liczb"
+git reset
+git revert
+git cherry-pick
+git rebase -i
+git tag
+git describe
+
+git clone
+git fetch
+git pull
+git faketeamwork
